81ace98d656ec0331e521c1ca04a86fda8d72056
jonatasjdb <jonatasjdb@hotmail.com>
2024-02-06 01:07:45 -0300

    For Luiza

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,8 @@ app.register(transactionsRoutes, {
   prefix: 'transactions',
 })
 
-// app.get('/Luiza', async (request, reply) => {
-//   return reply.send({
-//     'By':'Jônatas',
-
-//   })
-// })
+app.get('/Luiza', async (request, reply) => {
+  return reply.send(
+    'I love u one million times!\nThe day is comming!\n14/09/2024\n\nOBS: Ainda não fiz o curso de layout kkk',
+  )
+})
